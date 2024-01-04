46bc2c8512870be9c00dd0588ed087e45eb8a287
Elano Silva <elano_16@yahoo.com.br>
2024-01-04 12:25:25 -0300

    Finalizando projeto com JS

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+const navBar =  document.querySelector('.navbar')
+const allLi =  document.querySelectorAll('li')
+
+allLi.forEach((li, index) => {
+    li.addEventListener("click", e => {
+        navBar.querySelector(".active-list").classList.remove("active-list");
+        li.classList.add("active-list");
 
 
+        const indicator =  document.querySelector(".indicator");
+        indicator.style.transform = `translateX(calc(${index * 90}px))`;
+    });
+    
+});
